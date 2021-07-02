9db37ce5b8bbce45cab51b079f5100c2a3ccce01
socr <socr@DESKTOP-EIJQNVH>
2021-07-03 04:55:57 +0900

    update

--- custom.js
+++ custom.js
@@ -114,7 +114,7 @@ function checkCookie() {
                     <div class="direct-chat-msg">
                         <div class="direct-chat-info clearfix"> 
                             <span class="direct-chat-name pull-left">
-                                mazamedi@Chatbot
+                                mazamamedi@Chatbot
                             </span> 
                             <span class="direct-chat-timestamp pull-right">
                                 23 Jan 2:00 pm
