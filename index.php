7bc4eb8fc2aec76891f99df3639d3a048987f74d
socr102 <normanburtonfree@gmail.com>
2021-07-26 15:05:23 +0000

    update

---
+++ index.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset='utf-8'>
+    <meta http-equiv='X-UA-Compatible' content='IE=edge'>
+    <title>Web Messenger</title>
+    <meta name='viewport' content='width=device-width, initial-scale=1'>
+    <link rel='stylesheet' type='text/css' href='https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css'>
+    <link rel='stylesheet' type='text/css' href='https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.0.3/css/font-awesome.css'>
+    <link rel="stylesheet" type="text/css" href="custom.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+</head>
+<body>
+    <div class="page-content page-container" id="page-content">
+        <div class="padding">
+            <div class="row container d-flex justify-content-center">
+                <div style="width:370px">
+                    <div class="box box-warning direct-chat direct-chat-warning" style="height: 550px;">
+                        <div class="box-header with-border">
+                            <h3 class="box-title">Chat Messages</h3>
+                            <div class="box-tools pull-right"> 
+                                
+                            </div>
+                        </div>
+                        <div class="box-body">
+                            <div class="direct-chat-messages" id = "direct-chat-messages">
+                                
+                            </div> 
+                            <div id = "row8"></div>
+                        </div>
+                        <div class="box-footer">
+                            <!-- <form> -->
+                                <div class="input-group"> 
+                                    <input type="text" id="message" name="message" placeholder="Type Message ..." class="form-control"> 
+                                    <span class="input-group-btn"> 
+                                        <button type="submit" class="btn btn-warning btn-flat" id = "sendMessage">
+                                            Send
+                                        </button> 
+                                    </span> 
+                                </div>
+                            <!-- </form> -->
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</body>
+    <script src='https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js'></script>
+    <script src='https://stackpath.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.bundle.min.js'></script>
+    <script src="custom.js"></script>
+</html>
